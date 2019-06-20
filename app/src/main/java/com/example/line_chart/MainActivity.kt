b08b65ec5999f4c3198eca65fbf50965feb561fd
SANDBOB <sakul.s2557@gmail.com>
2019-06-20 14:43:30 +0700

    Update Code

--- app/src/main/java/com/example/line_chart/MainActivity.kt
+++ app/src/main/java/com/example/line_chart/MainActivity.kt
@@ -1,6 +1,6 @@
 package com.example.line_chart
 
-import android.graphics.Color
+
 import android.graphics.Color.rgb
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
@@ -36,28 +36,16 @@ class MainActivity : AppCompatActivity() {
             yVals.add(Entry(x.get(i),y.get(i)))
         }
 
-//        yVals.add(Entry(1f, 55f))
-//        yVals.add(Entry(2f, 60f))
-//        yVals.add(Entry(3f, 53f))
-//        yVals.add(Entry(4f, 50f))
-//        yVals.add(Entry(5f, 45f))
 
         val set1: LineDataSet
         set1 = LineDataSet(yVals, "DataSet 1")
 
-        // set1.fillAlpha = 110
-        // set1.setFillColor(Color.RED);
-
-        // set the line to be drawn like this "- - - - - -"
-        // set1.enableDashedLine(5f, 5f, 0f);
-        // set1.enableDashedHighlightLine(10f, 5f, 0f);
         set1.color = rgb(37, 231, 217)
         set1.setCircleColor(rgb(37, 231, 217))
         set1.lineWidth = 1f
         set1.circleRadius = 5f
         set1.setDrawCircleHole(false)
         set1.valueTextSize = 10f
-        //set1.setDrawFilled(false)
         set1.valueTextColor = rgb(37, 231, 217)
 
 
@@ -67,16 +55,9 @@ class MainActivity : AppCompatActivity() {
 
         // set data
         val week = arrayOf("","1", "2", "3", "4","5")
-//        val formatter = IAxisValueFormatter{
-//            value, axis ->   months[value.toInt()]
-//        }
-
         lineChart.xAxis.granularity = 1f
         lineChart.xAxis.axisLineWidth = 2f
         lineChart.xAxis.axisLineColor = rgb(211,211,211)
-        //lineChart.xAxis.axisMinimum = 0f
-        //lineChart.xAxis.axisMaximum = 6f
-        //lineChart.axisLeft.granularity = 10f
         lineChart.xAxis.spaceMax = 1f
         lineChart.xAxis.spaceMin = 1f
         lineChart.xAxis.valueFormatter = IndexAxisValueFormatter(week)
@@ -93,29 +74,12 @@ class MainActivity : AppCompatActivity() {
         lineChart.setData(data)
         lineChart.description.isEnabled = false
         lineChart.legend.isEnabled = false
-        //lineChart.setPinchZoom(true)
-        //lineChart.setDrawGridBackground()
-        //lineChart.xAxis.labelCount = 5
         lineChart.xAxis.position = XAxis.XAxisPosition.BOTTOM
         lineChart.xAxis.setDrawGridLines(false)
-        //lineChart.xAxis.isEnabled = false
         lineChart.axisRight.isEnabled = false
         lineChart.setTouchEnabled(false)
-        //lineChart.setClickable(false)
         lineChart.setExtraOffsets(10f,20f,20f,10f)
 
     }
 
-//    object WeekFormatter : IAxisValueFormatter {
-//
-//        override fun getFormattedValue(value: Float, axis: AxisBase?): String =   when  (value) {
-//            1f -> "Week1"
-//            2f -> "Week2"
-//            3f -> "Week3"
-//            4f -> "Week4"
-//            5f -> "Week5"
-//            else -> ""
-//        }
-//
-//    }
 }
